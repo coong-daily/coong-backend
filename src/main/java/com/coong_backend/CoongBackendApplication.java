756edd06fe71dff686feba8e77c850b67f5f8c25
남가빈 <cabin4nam@naver.com>
2026-06-24 22:10:59 +0900

    feat : Initialize Spring Boot Project

---
+++ src/main/java/com/coong_backend/CoongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coong_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoongBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoongBackendApplication.class, args);
+	}
+
+}
